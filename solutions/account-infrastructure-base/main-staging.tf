a676e9c0348d14addca9c3929f0407fdd65166a0
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-01-16 15:04:49 -0500

    add do nothing terraform for ys1-staging testing only

---
+++ solutions/account-infrastructure-base/main-staging.tf
@@ -0,0 +1,156 @@
+
+resource "time_sleep" "wait_5_seconds" {
+    create_duration = "5s"
+}
+
+output "security_resource_group_id" {
+    value       = "security_resource_group_id"
+}
+
+output "security_resource_group_id" {
+    value       = "security_resource_group_id"
+}
+
+output "audit_resource_group_id" {
+    value       = "audit_resource_group_id"
+}
+
+output "audit_resource_group_name" {
+    value       = "audit_resource_group_name"
+}
+
+output "observability_resource_group_id" {
+    value       = "observability_resource_group_id"
+}
+
+output "observability_resource_group_name" {
+    value       = "observability_resource_group_name"
+}
+
+output "management_resource_group_id" {
+    value       = "management_resource_group_id"
+}
+
+output "management_resource_group_name" {
+    value       = "management_resource_group_name"
+}
+
+output "workload_resource_group_id" {
+    value       = "workload_resource_group_id"
+}
+
+output "workload_resource_group_name" {
+    value       = "workload_resource_group_name"
+}
+
+output "edge_resource_group_id" {
+    value       = "edge_resource_group_id"
+}
+
+output "edge_resource_group_name" {
+    value       = "edge_resource_group_name"
+}
+
+output "devops_resource_group_id" {
+    value       = "devops_resource_group_id"
+}
+
+output "devops_resource_group_name" {
+    value       = "devops_resource_group_name"
+}
+
+output "activity_tracker_routes" {
+    value       = "activity_tracker_routes"
+}
+
+output "activity_tracker_targets" {
+    value       = "activity_tracker_targets"
+}
+
+output "cos_bucket" {
+    value       = "cos_bucket"
+}
+
+output "cos_instance_guid" {
+    value       = "cos_instance_guid"
+}
+
+output "cos_instance_id" {
+    value       = "cos_instance_id"
+}
+
+output "account_allowed_ip_addresses" {
+    value       = "account_allowed_ip_addresses"
+}
+
+output "account_allowed_ip_addresses_control_mode" {
+    value       = "account_allowed_ip_addresses_control_mode"
+}
+
+output "account_allowed_ip_addresses_enforced" {
+    value       = "account_allowed_ip_addresses_enforced"
+}
+
+output "account_iam_access_token_expiration" {
+    value       = "account_iam_access_token_expiration"
+}
+
+output "account_iam_active_session_timeout" {
+    value       = "account_iam_active_session_timeout"
+}
+
+output "account_iam_apikey_creation" {
+    value       = "account_iam_apikey_creation"
+}
+
+output "account_iam_inactive_session_timeout" {
+    value       = "account_iam_inactive_session_timeout"
+}
+
+output "account_iam_mfa" {
+    value       = "account_iam_mfa"
+}
+
+output "account_iam_refresh_token_expiration" {
+    value       = "account_iam_refresh_token_expiration"
+}
+
+output "account_iam_serviceid_creation" {
+    value       = "account_iam_serviceid_creation"
+}
+
+output "account_iam_user_mfa_list" {
+    value       = "account_iam_user_mfa_list"
+}
+
+output "account_public_access" {
+    value       = "account_public_access"
+}
+
+output "account_shell_settings_status" {
+    value       = "account_shell_settings_status"
+}
+
+output "trusted_profile_projects" {
+    value       = "trusted_profile_projects"
+}
+
+output "trusted_profile_projects_claim_rules" {
+    value       = "trusted_profile_projects_claim_rules"
+}
+
+output "trusted_profile_projects_links" {
+    value       = "trusted_profile_projects_links"
+}
+
+output "trusted_profile_projects_policies" {
+    value       = "trusted_profile_projects_policies"
+}
+
+output "cbr_map_service_ref_name_zoneid" {
+    value       = "cbr_map_service_ref_name_zoneid"
+}
+
+output "cbr_map_target_service_rule_ids" {
+    value       = "cbr_map_target_service_rule_ids"
+}
